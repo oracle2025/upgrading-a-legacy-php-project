04c4b2276e728ee197ee528c92a31eb48f9f1dd8
Richard Spindler <richard.spindler@gmail.com>
2021-05-16 08:07:20 +0200

    changes from Richards-Air.fritz.box on So 16 Mai 2021 08:07:20 CEST

--- download_php_unit.php
+++ download_php_unit.php
@@ -1,6 +1,9 @@
 <?
+	//'php:5.3', 'php:5.6
 	$url = "https://phar.phpunit.de/phpunit-4.phar";
 	$file = "phpunit";
+
+	echo 'Current PHP version: ' . phpversion() . "\n";
 
 	if (file_put_contents($file, file_get_contents($url))) {
 		echo "Success";
